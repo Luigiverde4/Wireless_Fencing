08421d68adc4b5d68604c5e56c7bd56eb3e29d67
R.Roman <luigiverdeyt@gmail.com>
2024-07-30 17:43:57 +0200

    Red WiFi creada
    Se crea el wifi y muestra los requests

--- Server_maquina/Server_maquina.ino
+++ Server_maquina/Server_maquina.ino
@@ -1,9 +1,49 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
 
+// Datos WiFi
+const char* ssid = "ESP32_Server";
+const char* password = "12345678";
+
+// Creamos un servidor en el puerto 80
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  
+  // Configuramos el ESP32 como Access Point
+  WiFi.softAP(ssid, password);
+
+  // Obtenemos la dirección IP del AP
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("Dirección IP del AP: ");
+  Serial.println(myIP);
+
+  // Iniciamos el servidor
+  server.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Comprobamos si se ha conectado algún cliente
+  WiFiClient client = server.available();
 
+  if (client) { // Se ha conectado alguien
+    Serial.print("IP Cliente conectado: ");
+    IPAddress IPCliente = client.remoteIP();
+    Serial.println(IPCliente);
+
+    // Procesar la solicitud del cliente aquí
+    // Por ejemplo, leer datos del cliente
+    while (client.connected()) {
+      if (client.available()) {
+        String request = client.readStringUntil('\r');
+        Serial.print("Request: ");
+        Serial.println(request);
+        client.flush();
+      }
+    }
+
+    // Cerrar la conexión
+    client.stop();
+    Serial.println("Cliente desconectado");
+  }
 }
